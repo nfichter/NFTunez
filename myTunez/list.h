11af22cee69a4cb09a35a8a992d8f5a7b724f9c5
nfichter <nfichter@stuy.edu>
2016-10-20 23:03:09 -0400

    Getting error in print_list on actual printing line (21)

---
+++ myTunez/list.h
@@ -0,0 +1,25 @@
+typedef struct song_node {
+  char name[256];
+  char artist[256];
+  struct song_node *next;
+} song_node;
+
+void lower_string(char s[]);
+song_node * insert_front_node(song_node *front, song_node *insert); 
+song_node * insert_node(song_node *prev, song_node *new);
+void print_list(song_node *list);
+song_node * first_song_by_name(song_node *front, char nameGiven[]);
+song_node * first_song_by_artist(song_node *front, char artistGiven[]);
+song_node * random_node(song_node *front);
+void remove_node(song_node *front, song_node *remove);
+void free_all(song_node *front);
+
+song_node * add_song(song_node *lib, char nameGiven[], char artistGiven[]); 
+song_node * search_for_name(song_node *lib, char nameGiven[]);
+song_node * search_for_artist(song_node *lib, char artistGiven[]); 
+void print_all_under_letter(song_node *lib, char letterGiven);
+void print_all_under_artist(song_node *lib, char artistGiven[]);
+void print_library(song_node *lib);
+void shuffle(song_node *lib);
+void remove_song(song_node *lib, char nameGiven[], char artistGiven[]);
+void delete_all(song_node *lib);
--- myTunez/songs.c
+++ myTunez/songs.c
@@ -1,68 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "songs.h"
+#include <time.h>
 
-void lower_string(char s[]) {
-   int c = 0;
-   while (s[c] != '\0') {
-      if (s[c] >= 'A' && s[c] <= 'Z') {
-         s[c] = s[c] + 32;
-      }
-      c++;
-   }
+#include "list.h"
+
+//ALL LIST FUNCTIONS//
+song_node * insert_front_node(song_node *front, song_node *insert) {
+  insert->next = front;
+  return insert;
 }
 
-song_node * insert_front(song_node *front, char nameGiven[], char artistGiven[]) {
-  song_node *newFront = (song_node*)malloc(sizeof(song_node));
-  
-  char nameLower[256];
-  char artistLower[256];
-  strncpy(nameLower,nameGiven,256);
-  strncpy(artistLower,artistGiven,256);
-  lower_string(nameLower);
-  lower_string(artistLower);
-
-  strncpy(newFront->name,nameLower,256);
-  strncpy(newFront->artist,artistLower,256);
-  newFront->next = front;
-
-  return newFront;
-}
-
-song_node * insert_song(song_node *front, char nameGiven[], char artistGiven[]) {
-  song_node *current = front;
-
-  char nameLower[256];
-  char artistLower[256];
-  strncpy(nameLower,nameGiven,256);
-  strncpy(artistLower,artistGiven,256);
-  lower_string(nameLower);
-  lower_string(artistLower);
-
-  //IF LIST ONLY HAS ONE ELEMENT
-  if (current->next == NULL) {
-    song_node *newNode = (song_node*)malloc(sizeof(song_node));
-    
-    strncpy(newNode->name,nameLower,256);
-    strncpy(newNode->artist,artistLower,256);
-
-    if (strcmp(current->artist,newNode->artist) < 0) {
-      current->next = newNode;
-    } else {
-      current = insert_front(current,newNode->name,newNode->artist);
-    }
-  }
-  else {
-    if (strcmp(current->next->artist,artistLower) < 0) {
-      current = insert_song(current->next,nameLower,artistLower);
-    }
-    else {
-      current = insert_front(current,nameLower,artistLower);
-    }
-  }
-
-  return current;
+song_node * insert_node(song_node *prev, song_node *new) {
+  song_node *temp = prev->next;
+  prev->next = new;
+  new->next = temp;
 }
 
 void print_list(song_node *list) {
@@ -108,7 +60,7 @@ song_node * first_song_by_artist(song_node *front, char artistGiven[]) {
   }
 }
 
-song_node * random_song(song_node *front) {
+song_node * random_node(song_node *front) {
   int length = 0;
   song_node * current = front;
 
@@ -116,27 +68,89 @@ song_node * random_song(song_node *front) {
     length++;
     current = current->next;
   }
+
+  srand(time(NULL));
+  int chosen = rand() % length;
+
+  current = front;
+  while (chosen) {
+    current = current->next;    
+    chosen--;
+  }
+
+  return current;
+}
+
+
+void remove_node(song_node *front, song_node *remove) {
+  song_node *current = front;
+
+  while (current != NULL) {
+    if (current->next == remove) {
+      current->next = remove->next;
+    }
+  }
+  free(remove);
+}
+
+void free_all(song_node *front) {
+  while (front != NULL) {
+    free(front);
+    front = front->next;
+  }
+}
+
+void lower_string(char s[]) {
+   int c = 0;
+   while (s[c] != '\0') {
+      if (s[c] >= 'A' && s[c] <= 'Z') {
+         s[c] = s[c] + 32;
+      }
+      c++;
+   }
+}
+
+//ALL SONG FUNCTIONS//
+song_node * add_song(song_node *lib, char nameGiven[], char artistGiven[]) {
+  char nameLower[256];
+  char artistLower[256];
+  strncpy(nameLower,nameGiven,256);
+  strncpy(artistLower,artistGiven,256);
+  lower_string(nameLower);
+  lower_string(artistLower);
+
+  song_node *newNode = (song_node*)malloc(sizeof(song_node));
+  strncpy(newNode->name,nameLower,256);
+  strncpy(newNode->artist,artistLower,256);
+
+  if (lib == NULL) {
+    return lib;
+  }
+  
+  if (strcmp(lib->artist,artistGiven) < 0) {
+    return insert_front_node(lib,newNode);
+  }
+
+  int added = 0;
+  song_node *current = lib;
+  while (current->next != NULL && added == 0) {
+    if (strcmp(current->next->artist,artistGiven) < 0) {
+      return insert_node(current,newNode);
+      added = 1;
+    }
+    current = current->next;
+  }
 }
 
 int main() {
-  song_node *list;
-  list = (song_node*)malloc(sizeof(song_node));
-  strcpy(list->name,"fdsa");
-  strcpy(list->artist,"fdsa");
-
-  list = insert_song(list,"ello","ello");
-  print_list(list);
-  
-  printf("\n");
-
-  list = insert_song(list,"ASDF","ASDF");
-  print_list(list);
-
-  song_node *find_ello = first_song_by_name(list,"ello");
-  printf("find_ello name: %s\n",find_ello->name);
-
-  song_node *find_fdsa = first_song_by_artist(list,"fdsa");
-  printf("find_fdsa artist: %s\n",find_fdsa->artist);
+  song_node *lib = (song_node*)malloc(sizeof(song_node));
+  lib = 0;
+  lib = add_song(lib,"eee","eee");
+  print_list(lib);
+  lib = add_song(lib,"ddd","ddd");
+  print_list(lib);
+  lib = add_song(lib,"fff","fff");
+  print_list(lib);
 
   return 0;
 }
